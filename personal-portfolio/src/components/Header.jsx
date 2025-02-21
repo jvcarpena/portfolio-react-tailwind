ca486a88898e6b8ef6b913beaa9c3e8b81d1fb33
jvcarpena <jvcarpena2@gmail.com>
2025-02-21 12:09:14 +0800

    Configured the tailwind in react and add a header

---
+++ personal-portfolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="sticky top-0 bg-[#d9d9d91f] z-10 shadow-md">
+      <section className="mx-auto flex max-w-7xl items-center justify-between p-4">
+        <h1 className="text-3xl font-medium text-white">
+          <a href="#hero">JV Carpena</a>
+        </h1>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
